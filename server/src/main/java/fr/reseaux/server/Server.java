791f0bcab3a544913f575f784cef7c6e1be65133
SoPlump <sophie.raudrant@gmail.com>
2019-11-24 14:37:46 +0100

    Adding a user list and a way to connect a user (not finished yet)

--- server/src/main/java/fr/reseaux/server/Server.java
+++ server/src/main/java/fr/reseaux/server/Server.java
@@ -1,11 +1,15 @@
 package fr.reseaux.server;
 
+import fr.reseaux.common.User;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
 import java.net.Inet4Address;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
+import java.util.List;
 
 public class Server {
 
@@ -14,6 +18,8 @@ public class Server {
     private static MulticastThread multicastThread;
 
     private static int multicastPort = 6789;
+
+    private static List<User> userList;
 
     public static void main(String[] args) {
         // launch javafx app
@@ -27,6 +33,12 @@ public class Server {
         }
 
         try {
+            // Create users
+            UserFactory userFactory = new UserFactory();
+            userList = userFactory.createUsersFromXML(new File("./src/main/resources/users.xml"));
+            LOGGER.debug("LISTE DES UTILISATEURS : " + userList);
+
+
             listenSocket = new ServerSocket(Integer.parseInt(args[0])); //port
             multicastThread = new MulticastThread(multicastPort, (Inet4Address)Inet4Address.getByName("225.225.225.225"));
             multicastThread.start();
@@ -37,10 +49,14 @@ public class Server {
                 System.out.println("Connexion from:" + clientSocket.getInetAddress());
                 ClientThread ct = new ClientThread(clientSocket);
                 ct.start();
-                //sl.addClient(clientSocket);
             }
         } catch (Exception e) {
             System.err.println("Error in Server:" + e);
+        }
+    }
+
+    static void connectUser(User user) {
+        if (userList.contains(user)) {
         }
     }
 
